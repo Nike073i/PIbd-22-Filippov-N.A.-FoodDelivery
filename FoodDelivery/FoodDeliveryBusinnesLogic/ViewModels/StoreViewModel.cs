5d31cb2b23d5b9640b5bc3b309311198d7e161bf
Nike073i <nike073i@mail.ru>
2021-02-18 20:37:36 +0400

    init

---
+++ FoodDelivery/FoodDeliveryBusinnesLogic/ViewModels/StoreViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FoodDeliveryBusinnesLogic.ViewModels
+{
+    public class StoreViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Название")]
+        public string StoreName { get; set; }
+
+        [DisplayName("ФИО ответственного")]
+        public string FullNameResponsible { get; set; }
+
+        [DisplayName("Дата создания")]
+        public DateTime CreationDate { get; set; }
+
+        public Dictionary<int, (string, int)> StoreDishes { get; set; }
+    }
+}
