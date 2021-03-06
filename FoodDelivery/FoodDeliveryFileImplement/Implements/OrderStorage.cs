fb7ab42b74fa9d3e0e5d15baebaf4dc2a8c17ee1
Nike073i <nike073i@mail.ru>
2021-03-06 18:14:09 +0400

    commit 3 6.3.21 18:13 refactoring code

--- FoodDelivery/FoodDeliveryFileImplement/Implements/OrderStorage.cs
+++ FoodDelivery/FoodDeliveryFileImplement/Implements/OrderStorage.cs
@@ -36,24 +36,24 @@ namespace FoodDeliveryFileImplement.Implements
         public void Insert(OrderBindingModel model)
         {
             int maxId = source.Orders.Count > 0 ? source.Orders.Max(rec => rec.Id) : 0;
-            var element = new Order { Id = maxId + 1 };
-            source.Orders.Add(CreateModel(model, element));
+            var newOrder = new Order { Id = maxId + 1 };
+            source.Orders.Add(CreateModel(model, newOrder));
         }
         public void Update(OrderBindingModel model)
         {
-            var element = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
-            if (element == null)
+            var order = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
+            if (order == null)
             {
                 throw new Exception("Заказ не найден");
             }
-            CreateModel(model, element);
+            CreateModel(model, order);
         }
         public void Delete(OrderBindingModel model)
         {
-            Order element = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
-            if (element != null)
+            Order order = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
+            if (order != null)
             {
-                source.Orders.Remove(element);
+                source.Orders.Remove(order);
             }
             else
             {
@@ -72,14 +72,8 @@ namespace FoodDeliveryFileImplement.Implements
         }
         private OrderViewModel CreateModel(Order order)
         {
-            string setName = null;
-            foreach (var set in source.Sets)
-            {
-                if (set.Id == order.SetId)
-                {
-                    setName = set.SetName;
-                }
-            }
+            var set = source.Sets.FirstOrDefault(rec => rec.Id == order.SetId);
+            string setName = set?.SetName;
             return new OrderViewModel
             {
                 Id = order.Id,
