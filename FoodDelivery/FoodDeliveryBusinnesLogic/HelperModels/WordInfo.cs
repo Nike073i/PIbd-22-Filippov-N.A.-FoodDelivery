6176069aa7896449d963c5c6325705e1bd2788f7
Nike073i <nike073i@mail.ru>
2021-03-18 21:33:52 +0400

    commit 1 18.03.2021 21:33 openXML add

---
+++ FoodDelivery/FoodDeliveryBusinnesLogic/HelperModels/WordInfo.cs
@@ -0,0 +1,12 @@
+﻿using FoodDeliveryBusinnesLogic.ViewModels;
+using System.Collections.Generic;
+
+namespace FoodDeliveryBusinnesLogic.HelperModels
+{
+    class WordInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<DishViewModel> Dishes { get; set; }
+    }
+}
